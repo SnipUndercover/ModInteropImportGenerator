e8ed0d8db2a77b69aa57e4a3933fd4a428af11f3
wuke32767 <2446214230@qq.com>
2025-07-06 07:42:29 +0800

    Implemented code fix

---
+++ ModInteropImportGenerator.Sample/CodeFixTest.cs
@@ -0,0 +1,60 @@
+﻿using ModInteropImportGenerator.Sample.Stubs;
+using MonoMod.ModInterop;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModInteropImportGenerator.Sample
+{
+    class Nest
+    {
+        [GenerateImports("CommunalHelper.DashStates")]
+        public static class DashStates1
+        {
+            #region DreamTunnel
+
+            public static int GetDreamTunnelDashState()
+            {
+                return 0;
+            }
+
+            public static bool HasDreamTunnelDash()
+            {
+                return false;
+            }
+
+            public static int GetDreamTunnelDashCount()
+            {
+                return 0;
+            }
+
+            public static ComponentStub DreamTunnelInteraction(
+                Action<PlayerStub> onPlayerEnter,
+                Action<PlayerStub> onPlayerExit)
+            {
+                return null;
+            }
+
+            #endregion
+
+            #region Seeker
+
+            public static bool HasSeekerDash()
+            {
+                return false;
+            }
+
+            public static bool IsSeekerDashAttacking()
+            {
+                return false;
+            }
+
+            #endregion
+        }
+
+    }
+}
