8a7a2668356b08164987a0fd30d9fc203b0e9691
wuke32767 <2446214230@qq.com>
2025-07-06 13:07:33 +0800

    supports nested class

--- ModInteropImportGenerator.Sample/CommunalHelperImports.cs
+++ ModInteropImportGenerator.Sample/CommunalHelperImports.cs
@@ -2,16 +2,18 @@ using System;
 using ModInteropImportGenerator.Sample.Stubs;
 
 namespace ModInteropImportGenerator.Sample;
-
-[GenerateImports("CommunalHelper.DashStates")]
-public static partial class DashStates
+partial class Sample
 {
-    public static partial int GetDreamTunnelDashState();
-    public static partial bool HasDreamTunnelDash();
-    public static partial int GetDreamTunnelDashCount();
-    public static partial ComponentStub DreamTunnelInteraction(
-        Action<PlayerStub> onPlayerEnter,
-        Action<PlayerStub> onPlayerExit);
-    public static partial bool HasSeekerDash();
-    public static partial bool IsSeekerDashAttacking();
+    [GenerateImports("CommunalHelper.DashStates")]
+    public static partial class DashStates
+    {
+        public static partial int GetDreamTunnelDashState();
+        public static partial bool HasDreamTunnelDash();
+        public static partial int GetDreamTunnelDashCount();
+        public static partial ComponentStub DreamTunnelInteraction(
+            Action<PlayerStub> onPlayerEnter,
+            Action<PlayerStub> onPlayerExit);
+        public static partial bool HasSeekerDash();
+        public static partial bool IsSeekerDashAttacking();
+    }
 }
