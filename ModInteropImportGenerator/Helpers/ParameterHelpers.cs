13bb674faadc687e96c459304ff4114a3ec2d1c7
SnipUndercover <snipundercover@gmail.com>
2025-07-07 01:22:32 +0200

    Use custom `SymbolDisplayFormat`

--- ModInteropImportGenerator/Helpers/ParameterHelpers.cs
+++ ModInteropImportGenerator/Helpers/ParameterHelpers.cs
@@ -5,37 +5,22 @@ namespace ModInteropImportGenerator.Helpers;
 
 internal static class ParameterHelpers
 {
-    internal static string GetDefinition(this IParameterSymbol parameter)
-    {
-        string? refKind = parameter.GetRefKindForDefinition();
-        string type = parameter.Type.ToDisplayString(SymbolDisplayFormat.MinimallyQualifiedFormat);
-        string name = parameter.Name;
+    private static readonly SymbolDisplayFormat ParameterReferenceDisplayFormat =
+        SymbolDisplayFormat.MinimallyQualifiedFormat
+            .WithParameterOptions(SymbolDisplayParameterOptions.IncludeName);
 
-        return refKind is not null
-            ? $"{refKind} {type} {name}"
-            : $"{type} {name}";
-    }
+    internal static string GetDefinition(this IParameterSymbol parameter)
+        => parameter.ToDisplayString(SymbolDisplayFormat.MinimallyQualifiedFormat);
 
     internal static string GetReference(this IParameterSymbol parameter)
     {
         string? refKind = parameter.GetRefKindForReference();
-        string name = parameter.Name;
+        string name = parameter.ToDisplayString(ParameterReferenceDisplayFormat);
 
         return refKind is not null
             ? $"{refKind} {name}"
             : name;
     }
-
-    internal static string? GetRefKindForDefinition(this IParameterSymbol parameter)
-        => parameter.RefKind switch {
-            RefKind.None => null,
-            RefKind.Ref => "ref",
-            RefKind.Out => "out",
-            RefKind.In => "in",
-            RefKind.RefReadOnlyParameter => "ref readonly",
-            _ => throw new NotSupportedException(
-                $"Parameter \"{parameter.Name}\" uses an unsupported {nameof(RefKind)}: {parameter.RefKind}"),
-        };
 
     internal static string? GetRefKindForReference(this IParameterSymbol parameter)
         => parameter.RefKind switch {
